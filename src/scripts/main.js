7d571b8f5e8b3112a0dffab39e35ae27528d6d7b
Anna Rodina <tavoshka@gmail.com>
2026-01-17 19:50:40 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -354,7 +354,26 @@ const people = [
   },
 ];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
+const tableBody = document.querySelector('.dashboard');
+people.forEach(person => {
+  const row = document.createElement('tr');
+  const age = person.died - person.born;
+  const century = Math.ceil(person.born / 100);
 
-// write your code here
+  const values = [
+  person.name,
+  person.sex === 'm' ? 'Male' : 'Female',
+  person.born,
+  person.died,
+  age,
+  century,
+];
+
+  values.forEach(value => {
+    const cell = document.createElement('td');
+    cell.textContent = value;
+    row.append(cell);
+  });
+
+  tableBody.append(row);
+})
